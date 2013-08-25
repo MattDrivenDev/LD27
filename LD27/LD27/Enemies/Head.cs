5c682c9298c6594453c2c02fdd33ac537b701cbe
saxonmatt <saxonmatt@gmail.com>
2013-08-25 20:52:53 +0100

    The Head

---
+++ LD27/LD27/Enemies/Head.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LD27.Enemies
+{
+    /// <summary>
+    /// The 'head' of @MattDrivenDev
+    /// </summary>
+    public class Head : Enemy
+    {
+        Vector3 Target;
+
+        public Head(Vector3 pos, Room room, VoxelSprite sprite)
+            : base(pos, room, sprite)
+        {
+            Target = new Vector3(room.World.X_SIZE * Voxel.SIZE, room.World.Y_SIZE * Voxel.SIZE, pos.Z);
+        }
+
+        public override void Update(GameTime gameTime, Room currentRoom, Hero gameHero, List<Door> doors)
+        {
+            if (currentRoom != Room) return;
+
+            if (Target.X < Position.X) Speed.X -= 0.01f;
+            if (Target.X > Position.X) Speed.X += 0.01f;
+            if (Target.Y < Position.Y) Speed.Y -= 0.01f;
+            if (Target.Y > Position.Y) Speed.Y += 0.01f;
+
+            base.Update(gameTime, currentRoom, gameHero, doors);
+        }
+    }
+}
