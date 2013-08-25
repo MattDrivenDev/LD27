d6d5e46c74f76b9119127d3d931416f67b1b3ada
Gareth Williams <gareth@garethw.net>
2013-08-25 20:16:27 +0100

    Head content

--- LD27/LD27/Enemies/Sentinel.cs
+++ LD27/LD27/Enemies/Sentinel.cs
@@ -8,10 +8,31 @@ namespace LD27
 {
     public class Sentinel : Enemy
     {
+        Vector3 Target;
+
         public Sentinel(Vector3 pos, Room room, VoxelSprite sprite)
             : base(pos, room, sprite)
         {
+            Target = new Vector3(room.World.X_SIZE * Voxel.SIZE, room.World.Y_SIZE * Voxel.SIZE, pos.Z);
+        }
 
+        public override void Update(GameTime gameTime, Room currentRoom, Hero gameHero, List<Door> doors)
+        {
+            if (currentRoom != Room) return;
+
+            if (Target.X < Position.X) Speed.X -= 0.01f;
+            if (Target.X > Position.X) Speed.X += 0.01f;
+            if (Target.Y < Position.Y) Speed.Y -= 0.01f;
+            if (Target.Y > Position.Y) Speed.Y += 0.01f;
+
+            base.Update(gameTime, currentRoom, gameHero, doors);
+        }
+
+        public override void DoCollide(bool x, bool y, bool z, Vector3 checkPosition, Room currentRoom, Hero gameHero, bool withPlayer)
+        {
+            Target = new Vector3(Room.World.X_SIZE * Voxel.SIZE, Room.World.Y_SIZE * Voxel.SIZE, Position.Z);
+
+            base.DoCollide(x, y, z, checkPosition, currentRoom, gameHero, withPlayer);
         }
     }
 }
